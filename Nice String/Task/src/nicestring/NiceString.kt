6dca05cffcc6bdd59db8afca5b43d028ca243c16
moranjani <morancha@gmail.com>
2019-11-25 16:43:58 +0200

    slightly better

--- Nice String/Task/src/nicestring/NiceString.kt
+++ Nice String/Task/src/nicestring/NiceString.kt
@@ -1,8 +1,8 @@
 package nicestring
 
 fun String.isNice(): Boolean {
-    val isFirstCondition = !"b[u,a,e]".toRegex().containsMatchIn(this)
-    val isSecondCondition = this.count { "aeiou".contains(it) }  >= 3
+    val isFirstCondition = listOf("ba", "be", "bu").none { this.contains(it) }
+    val isSecondCondition = count { "aeiou".contains(it) }  >= 3
     val isThirdCondition = this.zipWithNext().any { it.first == it.second }
 
     val result = listOf(isFirstCondition, isSecondCondition, isThirdCondition).count { it } >= 2
